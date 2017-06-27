381c19e0eaf9c5e648db6ad2faf9e814c00cda41
velis <velis.christodoulou@gmail.com>
2017-06-27 21:39:02 +0300

    initial load

---
+++ Bot Application1/Dialogs/LuisDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Luis.Models;
+using Universities_Data;
+using System.Text;
+
+namespace Bot_Application1.Dialogs
+{
+    [LuisModel("ebba0822-5d77-4203-9b77-75355aecca95", "a6b1fb43ba384a698242ea1da1ed098a")]
+    [Serializable]
+    public class LuisDialog : LuisDialog<object>
+    {
+        [LuisIntent("Campus")]
+        public async Task GetSize(IDialogContext context, LuisResult result)
+        {
+            string university = "";
+            EntityRecommendation rec;
+            if (result.TryFindEntity("university", out rec))
+            {
+                university = rec.Entity;
+                await context.PostAsync($"The campus of {university} is ");
+            }
+            else
+            {
+                await context.PostAsync("Sorry no university found.");
+            }
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("AcceptanceRate")]
+        public async Task GetAccRate(IDialogContext context, LuisResult result)
+        {
+            string university = "";
+            EntityRecommendation rec;
+            if (result.TryFindEntity("university", out rec))
+            {
+                university = rec.Entity;
+                await context.PostAsync($"The acceptance rate of {university} is something");
+            }
+            else
+            {
+                await context.PostAsync("Sorry no university found.");
+            }
+            context.Wait(MessageReceived);
+        }
+        
+    }
+}
---
+++ Universities Data/Universities.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universities_Data
+{
+    public class Universities
+    {
+        private static Dictionary<string, double> AcceptanceRates;
+        private static Dictionary<string, string> CampusSize;
+        private static Dictionary<string, string> SatScoreRange;
+        private static Dictionary<string, string> ActScoreRange;
+        private static Dictionary<string, int> UndergradStudents;
+       // private static Dictionary<string, string> ContactInfo;
+
+        public Universities()
+        {
+            if(AcceptanceRates == null || AcceptanceRates.Count == 0)
+                InitializeAcceptanceRates();
+            if (CampusSize == null || CampusSize.Count == 0)
+                InitializeCampusSize();
+            if (SatScoreRange == null || SatScoreRange.Count == 0)
+                InitializeAcceptanceRates();
+            if (ActScoreRange == null || ActScoreRange.Count == 0)
+                InitializeActScoreRange();
+            if (UndergradStudents == null || UndergradStudents.Count == 0)
+                InitializeUndergradStudents();
+        //    if (ContactInfo == null || ContactInfo.Count == 0)
+          //      InitializeContactInfo();
+        }
+        public void InitializeAcceptanceRates()
+        {
+            AcceptanceRates = new Dictionary<string, double>();
+            AcceptanceRates.Add("Stanford", 4.8);
+            AcceptanceRates.Add("Harvard", 5.4);
+            AcceptanceRates.Add("MIT", 7.9);
+        }
+
+        public void InitializeCampusSize()
+        {
+            CampusSize = new Dictionary<string, string>();
+            CampusSize.Add("Stanford", "Large");
+            CampusSize.Add("Harvard", "Midsize");
+            CampusSize.Add("MIT", "Midsize");
+        }
+
+        public void InitializeSatScoreRange()
+        {
+            SatScoreRange = new Dictionary<string, string>();
+            SatScoreRange.Add("Stanford", "1460-1590");
+            SatScoreRange.Add("Harvard", "1480-1600");
+            SatScoreRange.Add("MIT", "1480-1580");
+        }
+
+        public void InitializeActScoreRange()
+        {
+            ActScoreRange = new Dictionary<string, string>();
+            ActScoreRange.Add("Stanford", "");
+            ActScoreRange.Add("Harvard", "");
+            ActScoreRange.Add("MIT", "");
+        }
+
+        public void InitializeUndergradStudents()
+        {
+            UndergradStudents = new Dictionary<string, int>();
+            UndergradStudents.Add("Stanford", 7019);
+            UndergradStudents.Add("Harvard", 7200);
+            UndergradStudents.Add("MIT", 4476);
+        }
+
+      //  public void InitializeContactInfo()
+      //  {
+     //       ContactInfo = new Dictionary<string, int>();
+       //     ContactInfo.Add("Stanford", 7019);
+         //   ContactInfo.Add("Harvard", 7200);
+           // ContactInfo.Add("MIT", 4476);
+        //}
+        
+    }
+}
